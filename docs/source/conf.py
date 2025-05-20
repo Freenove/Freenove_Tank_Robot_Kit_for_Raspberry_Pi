e3104e1561ecc5ca38e853f351a1a600c515f601
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-05-20 14:34:18 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@ import sys
 
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
-# os.system("rm -r freenove_Kit")
-# os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Tank_Robot_Kit_for_Raspberry_Pi freenove_Kit")
+os.system("rm -r freenove_Kit")
+os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Tank_Robot_Kit_for_Raspberry_Pi freenove_Kit")
 
 project = "fnk0077-docs"
 # <!!!BEGIN!!!>
